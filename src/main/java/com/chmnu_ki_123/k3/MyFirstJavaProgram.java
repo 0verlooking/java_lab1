dee30b7ce3b851c054fb66e5653a912daf1c4678
Viktor Shevchenko <pouhtlh@gmail.com>
2024-09-27 23:15:32 +0300

    Init

--- src/main/java/com/chmnu_ki_123/k3/MyFirstJavaProgram.java
+++ src/main/java/com/chmnu_ki_123/k3/MyFirstJavaProgram.java
@@ -2,11 +2,7 @@ package com.chmnu_ki_123.k3;
 
 public class MyFirstJavaProgram {
 
-    /* This is my first java program.
-     * This will print 'Hello World' as the output
-     */
-
     public static void main(String []args) {
-        System.out.println("Hello World"); // prints Hello World
+        System.out.println("Hello World");
     }
 }
